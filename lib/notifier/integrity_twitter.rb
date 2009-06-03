17e73619f82c0338cc34e562126678d19ec5e104
Kai Schlichting <kai.schlichting@samedi.de>
2009-06-03 13:35:44 +0200

    * using new twitter lib api  * more verbose tweet message

--- lib/notifier/integrity_twitter.rb
+++ lib/notifier/integrity_twitter.rb
@@ -6,18 +6,30 @@ module Integrity
   class Notifier
     class IntegrityTwitter < Notifier::Base
       
+      def self.notify_of_build(build, config)
+        # Only send tweet when build is failed
+        build.failed? ? super : true
+      end
+      
       def self.to_haml
         File.read File.dirname(__FILE__) / "config.haml"
       end
 
       def deliver!
-        p @config["email"]
-        p @config["pass"]
         httpauth = Twitter::HTTPAuth.new(@config["email"], @config["pass"])
         @tweet = Twitter::Base.new(httpauth)
-        @tweet.update(short_message)
+        @tweet.update(message) rescue nil
       end
       
+      def message
+        "#{build_status} | #{commit.project.name} - [committer: #{commit.author.name}] ( #{commit_url} )"
+      end
+      
+      private
+      
+      def build_status
+        commit.successful? ? 'GREEN' : 'FAIL!'
+      end
     end
   end
 end
